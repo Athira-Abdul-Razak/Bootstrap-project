fa2bc790e58f79081d459ff7a0db7abfe4138050
AthiraAbdul Razak <athira.ar@fayausa.com>
2024-02-02 09:37:07 +0530

    feat(core): set up a project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,34 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { provideRouter, RouterModule } from '@angular/router';
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ServicesComponent } from './services/services.component';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'services', component: ServicesComponent },
+  { path: 'footer', component: FooterComponent },
+];
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterModule,HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'sample';
 }
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(routes) 
+  ]
+});
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
-import { Routes } from '@angular/router';
+import { Route } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ServicesComponent } from './services/services.component';
+import { FooterComponent } from './footer/footer.component';
 
-export const routes: Routes = [];
+export const APP_ROUTES: Route[] = [{
+  path: '',
+  component: HomeComponent,
+},
+{ path: 'about', component: AboutComponent },
+{ path: 'home', component: HomeComponent },
+{ path: 'services', component: ServicesComponent },
+{ path: 'footer', component: FooterComponent },
+];
